b944d006d2df496a7b81f860d4418acdeb94d16a
Beatrice <beatrice@computerino-di-beis.homenet.telecomitalia.it>
2022-04-14 18:32:44 +0200

    Initial commit

---
+++ src/ShippingCompany.java
@@ -0,0 +1,8 @@
+public class ShippigCompany {
+    private String name;
+
+    void ship(){
+        System.out.print("Ci pensa Bartolini!");
+        // TODO mah qua anche niente però decidere visibilità
+    }
+}
