3c42d4ff86c53f9fde96b8977419a92f704eb646
beatricevangi <beatricevangi99@gmail.com>
2022-04-22 12:50:55 +0200

    first version

--- src/Supplier.java
+++ src/Supplier.java
@@ -6,9 +6,9 @@ public class Supplier implements Observer{
     private Storage s = null;
 
     @Override
-    public void update(Object obj) {
+    public void update(Object str) {
         System.out.println("Carico in arrivoo!");
-        sendItem(obj);
+        sendItem(str);
     }
     //https://medium.datadriveninvestor.com/design-patterns-a-quick-guide-to-observer-pattern-d0622145d6c2
     //https://www.journaldev.com/1739/observer-design-pattern-in-java
@@ -40,5 +40,6 @@ public class Supplier implements Observer{
             }
             s.getSuppDecoration(decorations);
         }
+        s.getSuppProducts();
     }
 }
